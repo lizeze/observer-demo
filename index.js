f9c2324e9e2dd47fabf48c67b4fae28554494f9a
zeze.li <lizeze.boy@gmail.com>
2021-03-04 15:30:17 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+function defineReactive(target, key, value, viewModel) {
+    // 深度监听
+    observer(value,viewModel)
+
+    // 核心 API
+    Object.defineProperty(target, key, {
+        get() {
+            return value
+        },
+        set(newValue) {
+            if (newValue !== value) {
+                // 深度监听
+                observer(newValue,viewModel)
+
+                // 设置新值
+                // 注意，value 一直在闭包中，此处设置完之后，再 get 时也是会获取最新的值
+                value = newValue
+
+                // 触发更新视图
+                viewModel.methods[key].call(viewModel)
+                console.log('更新了')
+                // updateView()
+
+            }
+        }
+    })
+}
+
+function observer(target, viewModel) {
+    if (typeof target !== 'object' || target === null) {
+        // 不是对象或数组
+        return target
+    }
+
+    // 污染全局的 Array 原型
+    // Array.prototype.push = function () {
+    //     updateView()
+    //     ...
+    // }
+
+    if (Array.isArray(target)) {
+        target.__proto__ = arrProto
+    }
+
+    // 重新定义各个属性（for in 也可以遍历数组）
+    for (let key in target) {
+        defineReactive(target, key, target[key], viewModel)
+    }
+}
